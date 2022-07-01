3765512c30b02aa92049792adf03fe88caa0b7ec
Enzo <mhlee@maxst.com>
2022-07-01 18:38:47 +0900

    partner 예제 추가

--- order/src/main/java/com/practice/order/domain/partner/Partner.java
+++ order/src/main/java/com/practice/order/domain/partner/Partner.java
@@ -1,5 +1,7 @@
 package com.practice.order.domain.partner;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.Entity;
@@ -13,4 +15,18 @@ public class partner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String partnerName;
+    private String businessNo;
+    private String email;
+
+    private Status status;
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Status{
+        ENABLE, DISABLE;
+        private final String description;
+    }
+
 }
