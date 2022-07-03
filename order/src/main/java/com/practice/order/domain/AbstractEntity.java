3abf99d3d6d5291c5992c166838ef923287c5673
Enzo <mhlee@maxst.com>
2022-07-03 17:11:24 +0900

    공통 속성 Entity 생성

---
+++ order/src/main/java/com/practice/order/domain/AbstractEntity.java
@@ -0,0 +1,22 @@
+package com.practice.order.domain;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass//상송받는 클래스에서 자동으로 createdAt, updatedAt 속성이 추가된다.
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity {
+
+    @CreationTimestamp
+    private ZonedDateTime createdAt;
+
+    @UpdateTimestamp
+    private ZonedDateTime updatedAt;
+}
--- order/src/main/java/com/practice/order/domain/partner/Partner.java
+++ order/src/main/java/com/practice/order/domain/partner/Partner.java
@@ -1,21 +1,32 @@
 package com.practice.order.domain.partner;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
+import java.time.ZonedDateTime;
 
 @Entity
 @Slf4j
+@Getter
+@NoArgsConstructor
+@Table(name = "partners")
 public class Partner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String partnerToken;
 
     private String partnerName;
     private String businessNo;
     private String email;
+
+    private ZonedDateTime createAt;
+    private ZonedDateTime updatedAt;
+
     @Enumerated(EnumType.STRING)
     private Status status;
 
@@ -26,4 +37,22 @@ public class Partner {
         private final String description;
     }
 
+    @Builder
+    public Partner(String name , String businessNO, String email){
+        if(name == null)throw new IllegalArgumentException();
+        this.partnerToken = "randomToken";
+        this.partnerName = name;
+        this.businessNo = businessNO;
+        this.email = email;
+        this.status = Status.ENABLE;
+        this.createAt = ZonedDateTime.now();
+        this.updatedAt = ZonedDateTime.now();
+    }
+    public void enable(){
+        this.status = Status.ENABLE;
+    }
+
+    public void disable(){
+        this.status = Status.DISABLE;
+    }
 }
