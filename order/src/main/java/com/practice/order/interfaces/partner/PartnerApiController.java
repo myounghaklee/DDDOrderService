1475cb97e04f2aef747e0c4b4ca8bcda97e4ac33
myounghaklee <leemh9207@ajou.ac.kr>
2022-07-04 16:09:12 +0900

    controller annotation 수정

--- order/src/main/java/com/practice/order/interfaces/partner/PartnerApiController.java
+++ order/src/main/java/com/practice/order/interfaces/partner/PartnerApiController.java
@@ -5,6 +5,7 @@ import com.practice.order.application.partner.PartnerFacade;
 import com.practice.order.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class PartnerApiController {
     private final PartnerFacade partnerFacade;
 
     @PostMapping
-    public CommonResponse registerPartner(@RequestBody @Valid PartnerDto.RegisterRequest request) {
+    public CommonResponse registerPartner(@RequestBody @Validated PartnerDto.RegisterRequest request) {
         var command = request.toCommand();
         var partnerInfo = partnerFacade.registerPartner(command);
         var response = new PartnerDto.RegisterResponse(partnerInfo);
