01219868c318423773b7f5d98b892138b6d164fd
myounghaklee <leemh9207@ajou.ac.kr>
2022-07-04 13:30:33 +0900

    partner Domain infrastructure층 서비스 구현 - exception 처리

---
+++ order/src/main/java/com/practice/order/infrastructure/PartnerReadImpl.java
@@ -0,0 +1,28 @@
+package com.practice.order.infrastructure;
+
+import com.practice.order.domain.partner.Partner;
+import com.practice.order.domain.partner.PartnerReader;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PartnerReadImpl implements PartnerReader {
+    private final PartnerRepository partnerRepository;
+
+    @Override
+    public Partner getPartner(Long partnerId) {
+        return partnerRepository.findById(partnerId)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Partner getPartner(String partnerToken) {
+        return partnerRepository.findByPartnerToken(partnerToken)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+}
