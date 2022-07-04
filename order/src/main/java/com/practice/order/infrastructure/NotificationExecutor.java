d975692ccb874699f8a489c210b53802175dcecc
myounghaklee <leemh9207@ajou.ac.kr>
2022-07-04 16:07:48 +0900

    partner 도메인 application insterface 구현

--- order/src/main/java/com/practice/order/infrastructure/NotificationExecutor.java
+++ order/src/main/java/com/practice/order/infrastructure/NotificationExecutor.java
@@ -1,21 +1,25 @@
 package com.practice.order.infrastructure;
 
 import com.practice.order.domain.Notification.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
 public class NotificationExecutor implements NotificationService {
 
     @Override
     public void sendEmail(String email, String title, String description) {
-        
+        log.info("email");
     }
 
     @Override
     public void sendKakao(String phoneNo, String description) {
-
+        log.info("kakao");
     }
 
     @Override
     public void sendSms(String phoneNo, String description) {
-
+        log.info("sms");
     }
 }
